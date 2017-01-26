0275a5f664d3e612dd3cde93109c3f14f19f7435
Robert Barron <rmbarron@google.com>
2017-01-26 14:27:14 -0800

    Cleaned formatting for v0.1

--- software/main.py
+++ software/main.py
@@ -24,7 +24,7 @@ from RPi import GPIO
 
 
 scancodes = {
-	# Scancode: ASCIICode
+  # Scancode: ASCIICode
   0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 
   8: u'7', 9: u'8', 10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP',
   15: u'TAB', 16: u'q', 17: u'w', 18: u'e', 19: u'r', 20: u't', 21: u'y', 
@@ -84,79 +84,79 @@ class Button(object):
         self._blink_value ^= 1
         GPIO.output(self.light_gpio, self._blink_value)
 
-	# removes the press events, allows the script to loop without breaking
+  # removes the press events, allows the script to loop without breaking
     def unpress(self):
-	    self.press_event.clear()
-	    self.pressed = False
+      self.press_event.clear()
+      self.pressed = False
 
     def _callback(self, channel):
-	    self.press_event.set()
+      self.press_event.set()
       self.pressed = True
 
 # looks at connected devices, returns device matching name given
 def get_scanner_device():
-	deviceName = '<NameOfUSBBadgeScanner>'
-	devices = [evdev.InputDevice(x) for x in evdev.list_devices()]
-	device = None
-	for dev in devices:
-		if str(dev.name) == deviceName:
-			device = dev
-			break
-	return device
+  deviceName = '<NameOfUSBBadgeScanner>'
+  devices = [evdev.InputDevice(x) for x in evdev.list_devices()]
+  device = None
+  for dev in devices:
+    if str(dev.name) == deviceName:
+      device = dev
+      break
+  return device
 
 # read_input listens solely to the RFID keyboard and returns the scanned badge
 def read_input(device):
-	rfid=''
-	capitalized = 0
-	for event in device.read_loop():
+  rfid=''
+  capitalized = 0
+  for event in device.read_loop():
     data = evdev.categorize(event)
     if event.type == evdev.ecodes.EV_KEY and data.keystate == 1:
-			#detects if the keyboard uses "LSHFT"
-			if data.scancode == 42:
-				capitalized = 1
-			if data.keycode == "KEY_ENTER":
+      #detects if the keyboard uses "LSHFT"
+      if data.scancode == 42:
+        capitalized = 1
+      if data.keycode == "KEY_ENTER":
         break
-			if data.scancode != 42:
-				if capitalized:
-					rfid += capscancodes[data.scancode]
-					capitalized ^= 1
-				else:
-					rfid += scancodes[data.scancode]
-	return rfid
+      if data.scancode != 42:
+        if capitalized:
+          rfid += capscancodes[data.scancode]
+          capitalized ^= 1
+        else:
+          rfid += scancodes[data.scancode]
+  return rfid
 
 def main():
-	GPIO.setmode(GPIO.BOARD)
-	evt = threading.Event()
-	red = Button(12, 29, evt)
-	blue = Button(15, 31, evt)
-	# configures power tail 
-	GPIO.setup(37, GPIO.OUT)
-	dev = get_scanner_device()
-	badge = None
-	while True:
-		if badge == None:
-			badge = read_input(dev)
-		if badge:
-			red.light_blink()
-			while True:
-				red.idle()
-				blue.idle()
-				if evt.wait(timeout=0.5):
-					if red.pressed and not blue.pressed:
-						red.light_on()
-						GPIO.output(37, 1)
-						break
-					if blue.pressed:
-						GPIO.output(37, 0)
-						red.light_off()
-						badge = None
-						red.unpress()
-						break
-			evt.clear()
-			if not blue.pressed:
-				evt.wait()
-			elif blue.pressed:
+  GPIO.setmode(GPIO.BOARD)
+  evt = threading.Event()
+  red = Button(12, 29, evt)
+  blue = Button(15, 31, evt)
+  # configures power tail 
+  GPIO.setup(37, GPIO.OUT)
+  dev = get_scanner_device()
+  badge = None
+  while True:
+    if badge == None:
+      badge = read_input(dev)
+    if badge:
+      red.light_blink()
+      while True:
+        red.idle()
+        blue.idle()
+        if evt.wait(timeout=0.5):
+          if red.pressed and not blue.pressed:
+            red.light_on()
+            GPIO.output(37, 1)
+            break
+          if blue.pressed:
+            GPIO.output(37, 0)
+            red.light_off()
+            badge = None
+            red.unpress()
+            break
+      evt.clear()
+      if not blue.pressed:
+        evt.wait()
+      elif blue.pressed:
         blue.unpress()
-			
+      
 if __name__ == '__main__':
-	main()
+  main()
