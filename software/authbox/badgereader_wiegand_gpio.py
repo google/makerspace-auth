caa4e702234b5e893577c9c57461be1472515d16
Tim Hatch <tim@timhatch.com>
2019-06-05 22:42:41 -0700

    Add tests and switch to queue for badgereader_wiegand.
    The queue actually makes the testing logic way easier.

--- software/authbox/badgereader_wiegand_gpio.py
+++ software/authbox/badgereader_wiegand_gpio.py
@@ -15,8 +15,16 @@
 """Wiegand based badge reader directly connected via GPIO
 """
 
-from authbox.api import BaseWiegandPinThread, GPIO
+from __future__ import print_function, division
+
 import time
+import Queue
+
+from authbox.api import BaseWiegandPinThread, GPIO
+
+
+DEFAULT_QUEUE_SIZE = 100  # more than enough for a scan
+DEFAULT_TIMEOUT_IN_MS = 15
 
 
 class WiegandGPIOReader(BaseWiegandPinThread):
@@ -31,76 +39,55 @@ class WiegandGPIOReader(BaseWiegandPinThread):
   physical numbering).
   """
 
-  # This is a class variable to store the bitstream transmitted by the reader.
-  # It is stored using a string to avoid byte ordering issues as well as to
-  # simplify its processing.  It was initially implemented using the
-  # "bitstream" package, but this then had a dependency on a C compiler and
-  # NumPy
-  bits = ''
-
-  def_timeout_in_ms = 15
-  timeout = def_timeout_in_ms
-
-  def __init__( self, event_queue, config_name, d0_pin, d1_pin, on_scan=None):
+  def __init__(self, event_queue, config_name, d0_pin, d1_pin, on_scan=None,
+               queue_size=DEFAULT_QUEUE_SIZE,
+               timeout_in_ms=DEFAULT_TIMEOUT_IN_MS):
     super(WiegandGPIOReader, self).__init__(
         event_queue, config_name, int(d0_pin), int(d1_pin))
     self._on_scan = on_scan
+    # The limited-size queue protects from a slow leak in case of deadlock, so
+    # we can detect and output something (just a print for now)
+    self.bitqueue = Queue.Queue(int(queue_size))
+    self.timeout_in_seconds = float(timeout_in_ms) / 1000
 
     if self._on_scan:
         GPIO.add_event_detect(self.d0_pin, GPIO.FALLING, callback=self.decode)
         GPIO.add_event_detect(self.d1_pin, GPIO.FALLING, callback=self.decode)
 
-
-  def _callback(self):
-    """Wrapper to queue events instead of calling them directly."""
-    if self._on_scan:
-      self.event_queue.put((self._on_scan, self))
-
   def decode(self, channel):
-      if channel == self.d0_pin:
-          self.bits = self.bits + "0"
-      elif channel == self.d1_pin:
-          self.bits = self.bits + "1"
-      self.timeout = self.def_timeout_in_ms
-
+    bit = "0" if channel == self.d0_pin else "1"
+    try:
+      self.bitqueue.put_nowait(bit)
+    except Queue.Full:
+      # This shouldn't happen.
+      print("{name} BUG: QUEUE FULL".format(name=self.__class__.__name__))
 
   def read_input(self):
     """
+    This thread will perform a blocking read.  If there are no bits coming in
+    the stream, this will actually wait for them to start coming in.
 
     Args:
       None
 
-    This thread will perform a blocking read.  If there are no bits coming in
-    the stream, this will actually hold in the timeout state.
-
     Returns:
       badge value as string
     """
-    ## this will currently have a race condition where two cards read back
-    ## to back as one giant card
-    while self.timeout > 0:
-        if self.bits:
-            self.timeout = self.timeout -1
+    # Wait for a first bit to come in, slightly better than a busy-wait
+    while self.bitqueue.empty():
         time.sleep(0.001)
 
-    if len(self.bits) > 1 and self.timeout == 0:
-        ## TODO: Add a "debug" flag, which will output these values to the
-        ## console
-        #print "Binary:",self.bits
-        #result = int(str(self.bits[1:26]),2)
-        #print('Bits is {} bits in length'.format(len(self.bits)))
-        #print('{:012X}'.format(int(self.bits[-25:-1], 2)))
-        #print('{0:30b}'.format(int(self.bits[-25:-1], 2)))
-        b = self.bits
-        self.bits = ''
-        # change to start=1 to avoid this calculation and the one in
-        # the return for this function
-        start = 0 - len(self.bits) + 1
-        # document the format of the string, noting that for readers
-        # which push a string longer than 8 characters 
-        self.timeout = self.def_timeout_in_ms
-        return '{:08X}'.format(int(b[start:-1], 2))
+    ## this will currently have a race condition where two cards read back
+    ## to back as one giant card
+    bits = []
+    while True:
+        try:
+            bit = self.bitqueue.get(timeout=self.timeout_in_seconds)
+        except Queue.Empty:
+            break
+        bits.append(bit)
 
+    return ''.join(bits)
 
   def run_inner(self):
     line = self.read_input()
