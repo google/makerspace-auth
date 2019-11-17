03148fdc9cbc97bf255a04f80c567428d1caecaf
Tim Hatch <tim@timhatch.com>
2019-11-17 14:47:16 -0800

    Move tests to a package, set up virtualenv

---
+++ software/authbox/tests/__main__.py
@@ -0,0 +1,15 @@
+from authbox.tests.test_api import ClassRegistryTest
+from authbox.tests.test_api import DispatcherTest
+from authbox.tests.test_api import MultiProxyTest
+from authbox.tests.test_api import SplitEscapedTest
+from authbox.tests.test_badgereader_hid_keystroking import BadgereaderTest
+from authbox.tests.test_badgereader_wiegand_gpio import BadgereaderWiegandGPIOTest
+from authbox.tests.test_config import ConfigTest
+from authbox.tests.test_config import RecursiveConfigParamLookupTest
+from authbox.tests.test_fake_gpio import FakeGPIOTest
+from authbox.tests.test_gpio_button import BlinkTest
+from authbox.tests.test_gpio_buzzer import BuzzerTest
+from authbox.tests.test_gpio_relay import RelayTest
+
+from unittest import main
+main()
